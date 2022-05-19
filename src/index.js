8b30770f019a326472da4e302ef7ebb172aff4d3
kmt2050 <keithmturnas@gmail.com>
2022-05-19 12:35:26 -0600

    first commit four-in-a-row

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import _ from 'lodash';
-
 function component() {
   const element = document.createElement('div');
 
-  //Lodash, now imported by this script
+  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
